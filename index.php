75ebe4b95c5b03814b432cc0ef2903e524a34d01
FelixA-GitHub <67863506+FelixA-GitHub@users.noreply.github.com>
2020-07-09 17:04:59 -0400

    Final Comments Updated

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <<?php
+//this will include the functions.php code to be used throughout index.php
 include ('inc/functions.php');
 
 ?>
@@ -10,17 +11,21 @@ include ('inc/functions.php');
   <meta http-equiv="refresh" content="20" />
   <title>Random Quotes</title>
   <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" href="normalize.css">
-  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
  <?php
+  //this will provide random background colors every time screen is refreshed
   $backgroundColor = getRandomQuote($colors);
   echo "<body style='background-color: $backgroundColor'>"
-?>
+  ?>
   <div class="container">
     <div id="quote-box">
-      <?php echo printQuote($quotes) ?>
+      <?php
+      //this will print random quotes from $quotes every time screen is refreshed
+      echo printQuote($quotes)
+      ?>
     </div>
     <button id="loadQuote" onclick="window.location.reload(true)" >Show another quote</button>
   </div>
